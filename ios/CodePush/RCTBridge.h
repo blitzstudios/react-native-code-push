46de6f7aa5f7b0342410ed8ef3eea24be7d3f616
张超 <676651931@qq.com>
2020-03-03 10:41:53 +0800

    s

--- ios/CodePush/RCTBridge.h
+++ ios/CodePush/RCTBridge.h
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
+#import <React/RCTBridge.h>
 
 @interface RCTBridge () // RN私有类 ，这里暴露他的接口
 
 - (void)executeSourceCode:(NSData *)sourceCode sync:(BOOL)sync;
 
 @end
- 
